9786ba6a68620ffb6ffda7b4b8f6854247181984
Balazs Zsubrits <zsubrits.balazs.1995@gmail.com>
2021-11-30 22:04:37 +0100

    rewrote it + hw

--- script.js
+++ script.js
@@ -26,28 +26,54 @@ functionName();
 
 */
 
-
 const input = (type, name, label) => {
     return `
-        <label>${label}</label>
-        <input type="${type}" name="${name}"">
-    `;
+        <div>
+            <label>${label}</label>
+            <input type="${type}" name="${name}">
+        </div>
+    `
 }
 
-const form = `
+const formElement = `
     <form id="form">
-        ${input('text','name','Keresztneved')}
-        ${input('file','profPics','Profilképed')}
-        ${input('email','email','Email cím')}
-        ${input('radio','ola','Szeretnél hírlvelet?')}
-        ${input('checkbox','bola','Elfogadod a felhsz. felt.')}
+        ${input("text","name","Keresztnév")}
+        ${input("file","profpics","Profilkép")}
+        ${input("email","email","Eamil cím")}
+        ${input("radio","ola","Szeretnél hírlevelet?")}
+        ${input("checkbox","bola","Elfogadod a felhasználási feltételeket?")}
+       <button>ok</button>
     </form>
 `
 
-const init = () => {
+const formSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    event.target.classList.add('submitted');
+}
 
+const inputEvent = (event) => {
+    if (event.target.name === 'name'){
+        console.log(event.target.value);
+        document.getElementById('inputValueContent').innerHTML = event.target.value;
+
+    };
+}
+
+const init = () => {
     const root = document.getElementById('root');
-    root.insertAdjacentHTML('beforeend', form);
+    root.insertAdjacentHTML('beforeend',formElement);
+    root.insertAdjacentHTML('beforeend',`
+    <div id='inputValueContent'></div>
+    `);
+
+    const form = document.getElementById('form');
+    form.addEventListener('submit',formSubmit);
+
+    const inputList = form.querySelectorAll('input');
+    for(const input of inputList) {
+        input.addEventListener('input',inputEvent)
+    };
 
 }
 
