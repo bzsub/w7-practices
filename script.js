d7bbfb284a8ccdcf074b3efc92eedea5a8f3ce0d
Balazs Zsubrits <zsubrits.balazs.1995@gmail.com>
2021-12-01 11:48:02 +0100

    1hour fix

--- script.js
+++ script.js
@@ -29,31 +29,55 @@ functionName();
 const input = (type, name, label) => {
     return `
         <div>
-            <label>${label}</label>
-            <input type="${type}" name="${name}">
+            <label for="${name}">${label}</label>
+            <input type="${type}" name="${name}" required>
+            
         </div>
     `
 }
 
+const select = (type, name, label,selectOptions) => {
+    let optionElements = '';
+    for (const option of selectOptions){
+        optionElements += `<option value="${option}">${option}</option>`
+    }
+    return `
+    <div>
+        <label for="${name}">${label}</label>
+        <select name="${name}" required>
+            ${optionElements}
+        </select>
+    </div>
+
+    `
+}
 const formElement = `
     <form id="form">
+        <h1>EPIC FORM</h1>
         ${input("text","name","Keresztnév")}
         ${input("file","profpics","Profilkép")}
-        ${input("email","email","Eamil cím")}
-        ${input("radio","ola","Szeretnél hírlevelet?")}
+        ${input("email","email","E-mail cím")}
+        ${input("checkbox","ola","Szeretnél hírlevelet kapni?")}
         ${input("checkbox","bola","Elfogadod a felhasználási feltételeket?")}
-       <button>ok</button>
+        ${select("select","where","Hol hallottál rólunk?",["internetről","ismerőstől","egyéb"])}
+        <br/>
+       <button>OK</button>
     </form>
 `
 
 const formSubmit = (event) => {
     event.preventDefault();
-    console.log(event);
-    event.target.classList.add('submitted');
+    const et = event.target;
+    console.log(et);
+    et.classList.add('submitted');
+    const etValue = et.querySelector(`select[name="where"]`).value;
+    console.log(etValue);
 }
 
 const inputEvent = (event) => {
-    if (event.target.name === 'name'){
+    const fName = document.querySelector(`input[name='firstName']`);
+    console.log(fName);
+    if (event.target.getAttribute('name') === 'name'){
         console.log(event.target.value);
         document.getElementById('inputValueContent').innerHTML = event.target.value;
 
